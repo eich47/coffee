1a829154fd0c905862e6c2fa9c04ae1d8a57bc51
Sergey <eich@tut.by>
2020-12-10 21:00:49 +0300

    прокрутка к якорю

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 import Slider from './slider'
 import 'bootstrap/js/dist/tab'
-
+import $ from 'jquery/dist/jquery.min'
 const sliderCoffee = new Slider('#slider-coffee')
 const sliderCombo = new Slider('#slider-combo')
+
+
+//прокрутка по якорю
+$('.nav-general').click(function(e) {
+  e.preventDefault()
+  var sectionTo = $(e.target).attr('href');
+  $('html, body').animate({
+    scrollTop: $(sectionTo).offset().top
+  }, 500);
+});
