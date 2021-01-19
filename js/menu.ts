e3fde2473a28dd56d78b85c2ad2af1ee305d0ca7
Sergey <eich@tut.by>
2021-01-19 17:05:41 +0300

    скрипт меню переписал на ts

--- js/menu.ts
+++ js/menu.ts
@@ -1,7 +1,12 @@
 export default class MobileMenu {
-  //bntMenu - кнопка которая вызывает показ меню
+  buttonMenu: HTMLButtonElement
   //menu - меню какое будем модифицировать
-  constructor(bntMenu, menu) {
+  menu: HTMLElement
+  mobileMenuClass: string
+  //bntMenu - кнопка которая вызывает показ меню
+  bntMenuClass: string
+
+  constructor(bntMenu: string, menu: string) {
     this.buttonMenu = document.querySelector(bntMenu)
     this.menu = document.querySelector(menu)
     this.mobileMenuClass = 'mobile-menu'
@@ -20,7 +25,7 @@ export default class MobileMenu {
   //его на мобильной версии предваритель стилизовав
   //и добавив необходимые классы
   cloneExistMenu() {
-    const navMenuClone = this.menu.cloneNode(true)
+    const navMenuClone = this.menu.cloneNode(true) as HTMLElement
     navMenuClone.classList.add(this.mobileMenuClass, 'd-none')
     document.body.insertAdjacentElement('afterbegin', navMenuClone)
   }
@@ -30,7 +35,8 @@ export default class MobileMenu {
       e.preventDefault()
       const menu = document.querySelector(`.${this.mobileMenuClass}`)
       menu.classList.toggle('show')
-      const bnt = e.target.closest(this.bntMenuClass)
+      const target = e.target as HTMLElement
+      const bnt = target.closest(this.bntMenuClass) as HTMLButtonElement
       bnt.classList.toggle('animate')
     })
   }
