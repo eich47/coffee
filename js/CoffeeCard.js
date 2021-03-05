1385724e5c4a26f837008d5972af74184b011d5a
eich47 <eich@tut.by>
2021-03-05 16:27:13 +0300

    feat: add CoffeCard class

---
+++ js/CoffeeCard.js
@@ -0,0 +1,52 @@
+export class CoffeeCard {
+  constructor({ id, urlToImg, price, title, description }) {
+    this.id = id;
+    this.urlToImg = urlToImg;
+    this.price = price;
+    this.title = title;
+    this.description = description;
+  }
+
+  generateCard() {
+    let template = "";
+    let card = document.createElement("div");
+    card.classList.add("coffee-card");
+    card.setAttribute("data-id", this.id);
+
+    //img block
+    template += `<div class="coffee-card__image">`;
+    if (
+      this.urlToImg &&
+      (template += `<img src="${this.urlToImg}" alt="coffee desc"></img>`)
+    )
+      template += `</div>`;
+
+    //content block
+    template += `<div class="coffee-card__content">`;
+    //name
+    this.title &&
+      (template += `<div class="coffee-card__name">${this.title}</div>`);
+
+    this.description &&
+      (template += `<p class="coffee-card__description">${this.description}</p>`);
+
+    this.price &&
+      (template += `<span class="coffee-card__price">${this.price}</span>`);
+
+    //description
+    //price
+    //action buttons
+    template += `<div class="coffee-card__action">
+      <button class="link-button link-button_buy">
+        Заказать
+      </button>
+      <button class="link-button">Подробнее</button>
+    </div>`;
+
+    template += `</div>`;
+
+    card.innerHTML = template;
+
+    return card;
+  }
+}
