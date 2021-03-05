6326dae73d0a0f8cdfb79c9630e9de9ea2f95657
eich47 <eich@tut.by>
2021-03-05 16:36:47 +0300

    feat: add source data for coffee section

---
+++ js/data.js
@@ -0,0 +1,46 @@
+export const data = {
+  coffee: [
+    {
+      id: 1,
+      urlToImg: "img/coffee/coffee.png",
+      price: "99.00",
+      title: "Кофе 1",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    },
+    {
+      id: 2,
+      urlToImg: "img/coffee/coffee1.png",
+      price: "99.00",
+      title: "Кофе 1",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    },
+    {
+      id: 3,
+      urlToImg: "img/coffee/coffee.png",
+      price: "199.00",
+      title: "Кофе 1",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    },
+    {
+      id: 4,
+      urlToImg: "img/coffee/coffee1.png",
+      price: "99.00",
+      title: "Кофе 1",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    },
+    {
+      id: 5,
+      urlToImg: "img/coffee/coffee1.png",
+      price: "299.00",
+      title: "Кофе 1",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    },
+    {
+      id: 6,
+      urlToImg: "img/coffee/coffee.png",
+      price: "99.00",
+      title: "Кофе 1",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    },
+  ],
+};
