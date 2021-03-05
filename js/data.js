54d9ea514d7b0883a546eb429809d870eaefe73c
eich47 <eich@tut.by>
2021-03-05 18:35:45 +0300

    feat: add data for combo set of coffee

--- js/data.js
+++ js/data.js
@@ -43,4 +43,36 @@ export const data = {
       description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
     },
   ],
+
+  combo: [
+    {
+      id: 1,
+      urlToImg: "img/coffee/coffee.png",
+      price: "99.00",
+      title: "Комбо набор 1",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, vitae?.",
+    },
+    {
+      id: 2,
+      urlToImg: "img/coffee/coffee1.png",
+      price: "99.00",
+      title: "Комбо набор 2",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    },
+    {
+      id: 3,
+      urlToImg: "img/coffee/coffee.png",
+      price: "199.00",
+      title: "Комбо набор 3",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    },
+    {
+      id: 4,
+      urlToImg: "img/coffee/coffee1.png",
+      price: "99.00",
+      title: "Комбо набор 3",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    },
+  ],
 };
