5f90512a2d4126d0f5eae7b0c0a5faceeba66981
eich47 <eich@tut.by>
2021-03-10 17:59:39 +0300

    feat: styles for cart component

--- js/CartModal.js
+++ js/CartModal.js
@@ -9,6 +9,7 @@ export class CartModal extends Modal {
   generatorContent() {
     let template = ``;
     this.content = document.createElement("div");
+    this.content.classList.add("coffee-modal");
     this.content.innerHTML = `<h2>Корзина</h2>`;
 
     this.content.append(this.cart);
